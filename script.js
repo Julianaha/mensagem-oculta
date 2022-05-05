5e3a8baa50160f387ebbe44109e2668a8a5f7da1
Juliana Souza <julianha01@gmail.com>
2022-05-05 15:03:37 -0300

    alteracoes cifra de cesar

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 
-var codigo = document.getElementById('codigo');
-var incrementoCifra = document.getElementById('incrementoCifra');
-var codifica = document.getElementById('codificar');
+var codigo = document.getElementById("codigo");
+var incrementoCifra = document.getElementById("incrementoCifra");
+var codifica = document.getElementById("codificar");
 var decodifica = document.getElementById('decodificar');
 var entrada = document.querySelector(".entrada");
 var saida = document.querySelector(".saida");
-var btn = document.getElementById('button');
-var cifraDeCesar = document.getElementById('cifraDeCesar');
-var x = document.getElementById('valorIncremento');
+var btn = document.getElementById("button");
+var cifraDeCesar = document.getElementById("cifraDeCesar");
+var incremento = document.getElementById("incremento");
+var msg = document.getElementById("msg");
 
 
 //adiconando incremento para cifra de cesar
-codigo.addEventListener('click', function () {
+codigo.addEventListener("change", function () {
     if (codigo.value == 'cifraDeCesar') {
-        incrementoCifra.style.display = 'flex'
-        incrementoCifra.innerHTML = '<label for="incremento"> Incremento:</label><input type="number" id="valorIncremento">'
-    } else if(codigo.value == 'base64') {
+        incrementoCifra.style.display = 'flex';
+        incrementoCifra.style.gap = '10px';
+
+    } else{
         incrementoCifra.style.display = 'none';
     }
 })
 
-
-//seleciona input radio
-codifica.addEventListener('click', function () {
+//seleciona input codificar ou decodificar
+codifica.addEventListener("click", function () {
     btn.textContent = "codificar mensagem";
     document.getElementById("myText").placeholder = "Codificar Mensagem!";
 });
@@ -32,35 +33,47 @@ decodifica.addEventListener("click", function () {
     document.getElementById("myText").placeholder = "Decodificar Mensagem!";
 });
 
-//botao codifica e decodifica base64
+//botao base64 codifica ou decodifica
 btn.addEventListener("click", function (e) {
     e.preventDefault()
     if (codigo.value == "base64" && btn.textContent == "codificar mensagem") {
         saida.value = btoa(entrada.value);
     } else if (btn.textContent == "decodificar mensagem" && codigo.value == "base64") {
         saida.value = atob(entrada.value);
-    }
+    } 
 
 })
 
-//cifra de cesar
-
-var desl = 3
-var tamAlf = 26
-
-btn.addEventListener('click', function(e) {
+//botao cifraDeCesar codifica ou decodifica
+btn.addEventListener("click", function(e) {
     e.preventDefault();
-    var msgCodificada = document.querySelector(".entrada").value
-    if (codigo.value == "cifra" && btn.textContent == "codificar mensagem") {
-        function cifra(x,msgCodificada) {
-            var mensagem = '';
-            for (var i = 0; i <= msgCodificada.length; i++) {
-                var text = ((msgCodificada.charCodeAt(i) - 65 + x.value) % 26) + 65;
-                mensagem += String.fromCharCode(text)
-            }
-
-            return saida.value = mensagem;
-        }
+    var msgEntrada = entrada.value;
+    var x = incremento.value;
+    if (codigo.value == "cifraDeCesar" && codifica.checked){
+      saida.value = cifra(msgEntrada, x);
+    }else if(codigo.value == "cifraDeCesar" && decodifica.checked) {
+        saida.value = decCifra(msgEntrada, x)
     }
-})
+});
 
+function cifra(msgEntrada, x) {
+    mensagem = ""
+    msgEntrada = msgEntrada.toUpperCase()
+    x = parseInt(x);
+    for (var i = 0; i <= msgEntrada.length - 1; i++) {
+        var msgCodificada = ((msgEntrada.charCodeAt(i) - 65 + x) % 26) + 65;
+        mensagem += String.fromCharCode(msgCodificada);
+    }
+    return mensagem;
+}
+
+function decCifra(msgEntrada, x) {
+    mensagem = ""
+    msgEntrada = msgEntrada.toUpperCase()
+    x = parseInt(x)
+    for (var i = 0; i <= msgEntrada.length - 1; i++) {
+        var msgCodificada = ((msgEntrada.charCodeAt(i) - 65 - x) % 26) + 65;
+        mensagem += String.fromCharCode(msgCodificada);
+    }
+    return mensagem;
+}
